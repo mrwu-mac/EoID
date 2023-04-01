5c8ab590c9265425aa49286442b3c6dc28066e75
Pradnesh Kalkar <52340022+PradKalkar@users.noreply.github.com>
2023-04-01 12:09:35 +0530

    Update to accomodate hico_uo_st

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,6 +7,7 @@ from .hico_ua_st_v1 import build as build_hico_ua_st_v1
 from .hico_ua_st_v2 import build as build_hico_ua_st_v2
 from .hico_uc_base import build as build_hico_uc_base
 from .hico_uc_st import build as build_hico_uc_st
+from .hico_uo_st import build as build_hico_uo_st
 from .vcoco import build as build_vcoco
 from .vcoco1 import build as build_vcoco1
 from .hvco import build as build_hvco
@@ -25,6 +26,8 @@ def build_dataset(image_set, args):
         return build_hico_uc_base(image_set, args)
     if args.dataset_file == 'hico_uc_st':
         return build_hico_uc_st(image_set, args)
+    if args.dataset_file == 'hico_uo_st':
+        return build_hico_uo_st(image_set, args)
     if args.dataset_file == 'vcoco':
         return build_vcoco(image_set, args)
     if args.dataset_file == 'vcoco1':
